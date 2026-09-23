e1ab4bd68375efeccb3744cd97f77c564bbc6369
abevitor <blueprates86@gmail.com>
2026-09-23 20:39:49 -0300

    Refatora a estrutura do projeto: altera a classe principal para Launcher, ajusta o gradle-wrapper para a versão 8.10 e melhora a configuração do jpackage.

--- app-desktop/src/main/java/com/seuapp/desktop/App.java
+++ app-desktop/src/main/java/com/seuapp/desktop/App.java
@@ -2,22 +2,23 @@ package com.seuapp.desktop;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main-view.fxml"));
-        Parent root = loader.load();
+    public void start(Stage stage) throws Exception {
 
-        Scene scene = new Scene(root, 520, 360);
+        FXMLLoader loader = new FXMLLoader(
+                App.class.getResource("/main-view.fxml")
+        );
 
-        primaryStage.setTitle("YouTube Converter");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle("Youtube Converter");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
---
+++ app-desktop/src/main/java/com/seuapp/desktop/Launcher.java
@@ -0,0 +1,8 @@
+package com.seuapp.desktop;
+
+public class Launcher {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+    
+}
