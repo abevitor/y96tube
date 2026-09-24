6117a89e5d87b73d0403c7d2b1d49ad4680e1841
abevitor <blueprates86@gmail.com>
2026-09-24 18:31:38 -0300

    Melhora a lógica de inicialização da aplicação: adiciona mensagens de log para o carregamento do FXML e ajusta o tratamento de exceções no método start.

--- app-desktop/src/main/java/com/seuapp/desktop/App.java
+++ app-desktop/src/main/java/com/seuapp/desktop/App.java
@@ -2,24 +2,48 @@ package com.seuapp.desktop;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
+public void start(Stage stage) {
+
+    try {
+
+        System.out.println("1 - iniciando App");
 
         FXMLLoader loader = new FXMLLoader(
                 App.class.getResource("/main-view.fxml")
         );
 
-        Scene scene = new Scene(loader.load());
+        System.out.println(
+                "2 - FXML encontrado: "
+                        + loader.getLocation()
+        );
+
+        Parent root = loader.load();
+
+        System.out.println("3 - FXML carregado");
+
+        Scene scene = new Scene(root);
 
         stage.setTitle("Youtube Converter");
         stage.setScene(scene);
+
+        System.out.println("4 - mostrando janela");
+
         stage.show();
+
+        System.out.println("5 - janela mostrada");
+
+    } catch (Exception e) {
+
+        e.printStackTrace();
     }
+}
 
     public static void main(String[] args) {
         launch(args);
