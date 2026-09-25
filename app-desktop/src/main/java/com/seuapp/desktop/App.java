fef7170729b602dd3231405c89c22470d79c5cf0
abevitor <blueprates86@gmail.com>
2026-09-25 10:10:27 -0300

    Refatora a inicialização da aplicação: remove mensagens de log desnecessárias e ajusta o tratamento de exceções no método start. Adiciona estilo à label do título no FXML e cria um novo arquivo de estilo CSS para a interface.

--- app-desktop/src/main/java/com/seuapp/desktop/App.java
+++ app-desktop/src/main/java/com/seuapp/desktop/App.java
@@ -9,41 +9,16 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-public void start(Stage stage) {
-
-    try {
-
-        System.out.println("1 - iniciando App");
-
-        FXMLLoader loader = new FXMLLoader(
-                App.class.getResource("/main-view.fxml")
-        );
-
-        System.out.println(
-                "2 - FXML encontrado: "
-                        + loader.getLocation()
-        );
-
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("/main-view.fxml"));
         Parent root = loader.load();
 
-        System.out.println("3 - FXML carregado");
-
-        Scene scene = new Scene(root);
-
+        Scene scene = new Scene(root, 520, 360);
+        scene.getStylesheets().add(getClass().getResource("./style.css").toExternalForm());
         stage.setTitle("Youtube Converter");
         stage.setScene(scene);
-
-        System.out.println("4 - mostrando janela");
-
         stage.show();
-
-        System.out.println("5 - janela mostrada");
-
-    } catch (Exception e) {
-
-        e.printStackTrace();
     }
-}
 
     public static void main(String[] args) {
         launch(args);
